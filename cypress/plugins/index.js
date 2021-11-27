1881b096c123a456541a49fe1c24a44cb7a721f1
DESKTOP-813ME2J\Raulg <raoul.rogojinaru@outlook.com>
2021-11-27 20:53:27 +0200

    Cypress+Cucumber+MochaReports(deleting oldOnes)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,10 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+const cucumber = require("cypress-cucumber-preprocessor").default
+module.exports=(on,config)=>{
+    on("file:preprocessor",cucumber())
+}
+// deleteReports.js
+const fs = require("fs");
+fs.rmdirSync("./cypress/report", {recursive: true});
